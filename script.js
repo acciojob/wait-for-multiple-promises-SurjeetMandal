0a9280819bea93cc4513f2063d303c9f6967031b
SurjeetMandal <surjeetmandal698@gmail.com>
2024-05-22 12:57:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,61 +1,55 @@
-function promise1(){
-	const time = 2;
-	return new Promise((resolve) => {
+const promises = [
+	new Promise((resolve) => {
+		const time1 = 2;
 		setTimeout(() => {
-			resolve({name:'Promise 1', time});
-		},time * 1000)
-	})
-}
-
-function promise2(){
-	const time = 1;
-	return new Promise((resolve) => {
+			resolve({name: 'Promise 1', time: time1});
+		}, time1 * 1000);
+	}),
+	new Promise((resolve) => {
+		const time2 = 1;
 		setTimeout(() => {
-			resolve({name:'Promise 2', time});
-		},time * 1000)
-	})
-}
-
-function promise3(){
-	const time = 3;
-	return new Promise((resolve) => {
+			resolve({name: 'Promise 2', time: time2});
+		}, time2 * 1000);
+	}),
+	new Promise((resolve) => {
+		const time3 = 3;
 		setTimeout(() => {
-			resolve({name:'Promise 3', time});
-		},time * 1000)
-	})
-}
+			resolve({name: 'Promise 3', time: time3});
+		}, time3 * 1000);
+	}),
+];
 
-let Promises = [promise1(),promise2(),promise3()];
 let tableBody = document.getElementById('output');
-let LoadingRow = document.getElementById('loading');
+let loadingRow = document.getElementById('loading');
 
-Promise.all(Promises)
-.then(data => {
+Promise.all(promises).then(data => {
+	tableBody.removeChild(loadingRow);
 
-	tableBody.removeChild(LoadingRow);
-	
-	data.forEach(items => {
+	let totalTime = 0;
 
+	data.forEach(item => {
 		let tr = document.createElement('tr');
 		let cell1 = document.createElement('td');
 		let cell2 = document.createElement('td');
 
-		cell1.textContent = items.name;
-		cell2.textContent = `${items.time}s`;
+		cell1.textContent = item.name;
+		cell2.textContent = `${item.time}s`;
 
 		tr.appendChild(cell1);
 		tr.appendChild(cell2);
 		tableBody.appendChild(tr);
+
+		totalTime += item.time;
 	});
-	
+
 	let tr = document.createElement('tr');
 	let cell1 = document.createElement('td');
 	let cell2 = document.createElement('td');
 
-	cell1.textContent = `Total`;
-	cell2.textContent = `6s`;
+	cell1.textContent = 'Total';
+	cell2.textContent = `${totalTime}s`;
 
 	tr.appendChild(cell1);
 	tr.appendChild(cell2);
 	tableBody.appendChild(tr);
-})
+});
